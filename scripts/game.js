d9990c9475f0cebda512ff6e3b28268558c0c663
Maya-Claveau <maya.claveau@gmail.com>
2022-04-25 13:04:58 +0000

    add lightsOn function and test

--- scripts/game.js
+++ scripts/game.js
@@ -9,11 +9,25 @@ function newGame() {
     game.score = 0;
     game.playerMoves = [];
     game.currentGame = [];
-    showScore()
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
